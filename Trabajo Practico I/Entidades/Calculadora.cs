93158cff9baf6a2f2d432131788f7dbf93b1ec78
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2020-04-04 17:22:49 -0300

    Agrego funcion ValidarOperador y funcion para testear

---
+++ Trabajo Practico I/Entidades/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        static string ValidarOperador(string operador)
+        {
+            string operadorRetornado = " ";
+
+            switch(operador)
+            {
+                case "+":
+                    operadorRetornado = operador;
+                    break;
+                case "-":
+                    operadorRetornado = operador;
+                    break;
+                case "/":
+                    operadorRetornado = operador;
+                    break;
+                case "*":
+                    operadorRetornado = operador;
+                    break;
+                default:
+                    operadorRetornado = "+";
+                    break;
+            }
+
+            return operadorRetornado;
+        }
+        public static void Testear()
+        {
+            string operadorIngresado;
+            string operadorRetornado;
+
+            Console.WriteLine("Ingrese un operador: +, -, /, * ");
+            operadorIngresado = Console.ReadLine();
+
+            operadorRetornado = ValidarOperador(operadorIngresado);
+
+            Console.WriteLine("El operador retornado es: " + operadorRetornado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
